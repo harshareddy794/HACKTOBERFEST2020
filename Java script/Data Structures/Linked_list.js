9aff7f264121725b7bd9e69bd9eb77579bbf9a12
palak yadav <palakpoorvee@gmail.com>
2020-10-02 19:50:13 +0530

    Added some of the Data Structures in JS

---
+++ Java script/Data Structures/Linked_list.js
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    <script>
+      class Node { //defining a JavaScript class
+          constructor(data) { //constructor method
+              this.data = data; //data value of the node
+              this.next = null; //link of the next node
+          }
+      }
+      const head = new Node(12); //creating a node
+    </script>
+  </body>
+</html>
