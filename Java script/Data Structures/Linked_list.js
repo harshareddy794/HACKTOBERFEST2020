6ee689c158e49a218ac7862ef7bbc32b86af11e6
Kartikexp <72144829+Kartikexp@users.noreply.github.com>
2020-10-06 02:18:33 +0530

    Revert "Update Linked_list.js" (#2)

--- Java script/Data Structures/Linked_list.js
+++ Java script/Data Structures/Linked_list.js
@@ -8,13 +8,6 @@
           }
       }
       const head = new Node(12); //creating a node
- function displayTime(){
-    let time = new Date();
-    console.log(time);
-    document.getElementById('time').innerHTML=time;
-    }
-    setInterval(displayTime , 1000)
-
     </script>
   </body>
 </html>
