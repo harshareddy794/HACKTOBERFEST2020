6960f2427e84bee1c7d4b967f275239686e5ab01
Kartikexp <72144829+Kartikexp@users.noreply.github.com>
2020-10-06 02:11:40 +0530

    Update Linked_list.js
    made a clock which displays time . Its not a unnecssary change and I put some valuable content so pls accept

--- Java script/Data Structures/Linked_list.js
+++ Java script/Data Structures/Linked_list.js
@@ -8,6 +8,13 @@
           }
       }
       const head = new Node(12); //creating a node
+ function displayTime(){
+    let time = new Date();
+    console.log(time);
+    document.getElementById('time').innerHTML=time;
+    }
+    setInterval(displayTime , 1000)
+
     </script>
   </body>
 </html>
