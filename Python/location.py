023652d560fc9aa4cb8fb03ae34460c9b0868eee
harshgaud2918 <54628284+harshgaud2918@users.noreply.github.com>
2020-10-05 11:12:14 +0530

    Finding Location Using JSON

---
+++ Python/location.py
@@ -0,0 +1,46 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+api_key= 42
+
+serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    #key=parameter
+    parms = dict()
+    parms['address'] = address
+    parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+
+
+    location = js['results'][0]['place_id']
+    print(location)
