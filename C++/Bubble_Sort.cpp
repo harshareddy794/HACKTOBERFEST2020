9da41a169de865d20e5d6c4aa3cff553d7328cb8
Prachi <prachi.17becef008@gmail.com>
2020-10-18 00:27:29 +0530

    added code for knapsack greedy algorithm
    I have created cpp code for knapsack problem solved using greedy algorithm from ADA.

--- C++/Bubble_Sort.cpp
+++ C++/Bubble_Sort.cpp
@@ -1,33 +1,41 @@
+#include<iostream>
 using namespace std;
-
-int main()
-{
-    int n, i, j, temp;
-    cout << "Enter the number of elements" << endl;
-    cin >> n;
-    int arr[n];
-    for (i = 0; i < n; i++)
-    {
-        cout << "Enter " << i + 1 << " th element";
-        cin >> arr[i];
-    }
-
-    for (i = n; i > 0; i--)
-    {
-        for (j = 0; j < i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    cout << "\nSorted Elements are\n";
-    for (i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    return 0;
+void swapping(int &a, int &b) {      //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void bubbleSort(int *array, int size) {
+   for(int i = 0; i<size; i++) {
+      int swaps = 0;         //flag to detect any swap is there or not
+      for(int j = 0; j<size-i-1; j++) {
+         if(array[j] > array[j+1]) {       //when the current item is bigger than next
+            swapping(array[j], array[j+1]);
+            swaps = 1;    //set swap flag
+         }
+      }
+      if(!swaps)
+         break;       // No swap in this pass, so array is sorted
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];     //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   bubbleSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
 }
